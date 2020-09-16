0c4bcca255c24548ad92f1b8930f4a15d98b772f
Michael Lewis <wmichael.lewis@outlook.com>
2020-09-16 00:53:01 -0700

    adding Jquery for Modal pop ups

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,32 @@
+//Reserve button jquery code
+
+$(function () {
+
+    // Attach Button click event listener 
+   $("#reserveButton").click(function(){
+
+        // show Modal
+        $("#reserveModal").modal("show");
+   });
+});
+
+
+// Login Modal Jquery code
+
+$(function () {
+
+    // Attach Button click event listener 
+   $("#loginButton").click(function(){
+
+        // show Modal
+        $("#loginModal").modal("show");
+   });
+});
+    
+    
+    
+    
+    
     $(function() {
         $(".carousel").carousel( { interval: 2000 } );
         $("#carouselButton").click(function(){
@@ -12,3 +41,54 @@
             }
         });
     });
+
+
+
+        //Button ID's reserveButton | loginButton
+      
+
+
+
+    /* Workshop code theory
+
+        //html
+
+        <!-- Update the code below this comment -->
+                <button class="btn btn-warning" id="btnShowBanana">Show Banana</button>
+                <button class="btn btn-success"id="btnShowMonkey">Show Monkey</button>
+                <button class="btn btn-warning" id="btnHideBanana">Hide Banana</button>
+                <button class="btn btn-success" id="btnHideMonkey">Hide Monkey</button>
+                <div>
+                <img  class='collapse'  src="https://i.ibb.co/82WT957/banana.png" id='banana' />
+                <img class='collapse'  src="https://i.ibb.co/VJDfnxN/monkey-w-banana.png" id='monkey' />
+                </div>
+                <!-- Update the code above this comment -->
+                <hr />
+                <p>This Code Challenge is intended to give you more practice with using jQuery to manipulate Bootstrap components.</p>
+                <p>
+        //End html
+
+    $(document).ready(function(){
+        $("#btnShowBanana").click(function(){
+          $('#banana').show();
+         });
+      });
+     
+     $(document).ready(function(){
+        $("#btnShowMonkey").click(function(){
+          $('#monkey').show();
+         });
+      });
+     
+     $(document).ready(function(){
+       $("#btnHideBanana").click(function(){
+         $('#banana').hide();
+       });
+     });
+     
+     $(document).ready(function(){
+       $("#btnHideMonkey").click(function(){
+         $('#monkey').hide();
+       });
+     });
+     */
